f4d205ff67e38d506cfb18f2185709af55e98c64
Héctor <119538139+mirandahectora@users.noreply.github.com>
2024-06-27 19:54:13 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -9,8 +9,8 @@
            ╚═╝    ╚═╝╚═╝  ╚═══╝     ╚═╝    
                                      
                                      
-     A package by Héctor Miranda Plaza
-           Yale University, 2024
+       A package by Héctor Miranda Plaza
+             Yale University, 2024
   
 *This Package and its contents are not affiliated
  with Minitab, LLC. or any of its properties.*
