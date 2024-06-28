857d4945ae3d6087b3cf30bf7853074791f4ede3
Héctor <119538139+mirandahectora@users.noreply.github.com>
2024-06-27 20:10:36 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,19 @@
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage("
-  
+
+
       ██╗  ██╗    ██╗███╗   ██╗     ██╗    
       ██║  ██║    ██║████╗  ██║    ███║    
       ███████║    ██║██╔██╗ ██║    ╚██║    
       ╚════██║    ██║██║╚██╗██║     ██║    
            ██║    ██║██║ ╚████║     ██║    
            ╚═╝    ╚═╝╚═╝  ╚═══╝     ╚═╝    
-                                     
-                                     
+                                    
        A package by Héctor Miranda Plaza
              Yale University, 2024
   
 *This Package and its contents are not affiliated
- with Minitab, LLC. or any of its properties.*
+  with Minitab, LLC. or any of its properties.*
                         
                         ")
 }
